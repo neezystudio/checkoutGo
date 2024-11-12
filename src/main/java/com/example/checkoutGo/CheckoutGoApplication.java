ce82f2fbba6d3d21ea6206116c18387463965a91
neeystudio <victorneez6@gmail.com>
2024-11-12 20:24:25 +0300

    initial commit

---
+++ src/main/java/com/example/checkoutGo/CheckoutGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkoutGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkoutGo/CheckoutGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkoutGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
